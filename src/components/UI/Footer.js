017340631ba9b89e271ee71742b2f47aa563f3a8
Kyle Langille <kyle.a.langille@gmail.com>
2022-10-25 06:58:24 -0230

    started using styled-components

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,11 +1,28 @@
 import React from "react";
-import "./UI.css";
+import styled from "styled-components";
+
+const Foot = styled.footer`
+  position: fixed;
+  bottom: 0;
+  height: 3rem;
+  background: #464444;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FooterText = styled.p`
+  color: #b3b3b3;
+  font-family: "Inter";
+  font-weight: 500;
+`;
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p className="footer-text">Made with ❤️ by Kyle</p>
-    </footer>
+    <Foot>
+      <FooterText>Made with ❤️ by Kyle</FooterText>
+    </Foot>
   );
 };
 
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./UI.css";
+import styled from "styled-components";
+
+const Head = styled.header`
+  height: 7rem;
+  background-color: #464444;
+  width: 100vw;
+  border-bottom: 1px solid #000;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+`;
+
+const MainHeading = styled.h1`
+  color: #b3b3b3;
+  font-size: 3rem;
+  font-family: "Inter";
+`;
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1 className="main-heading">📚MyLibrary</h1>
-    </header>
+    <Head>
+      <MainHeading>📚MyLibrary</MainHeading>
+    </Head>
   );
 };
 
